5116360e03988196c6c98ff8a148a2a836c114f6
Mayur Patil <Mayurpatilofficial10@gmail.com>
2025-08-16 16:32:04 +0530

    Update ContactForm.tsx

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,31 +2,31 @@ import React from "react";
 
 const ContactForm: React.FC = () => {
   return (
-    <form className="w-full max-w-full px-4 sm:px-0 grid grid-cols-1 gap-5">
+    <form className="col-span-10 lg:col-span-5 grid grid-cols-1 gap-5">
       <input
         type="text"
         placeholder="Full name"
-        className="w-full bg-transparent border-b border-white/50 focus:border-white outline-none py-3 placeholder-white/80 text-base"
+        className="bg-transparent border-b border-white/50 focus:border-white outline-none py-3 placeholder-white/80 text-base"
       />
       <input
         type="email"
         placeholder="Email"
-        className="w-full bg-transparent border-b border-white/50 focus:border-white outline-none py-3 placeholder-white/80 text-base"
+        className="bg-transparent border-b border-white/50 focus:border-white outline-none py-3 placeholder-white/80 text-base"
       />
       <input
         type="text"
         placeholder="Company"
-        className="w-full bg-transparent border-b border-white/50 focus:border-white outline-none py-3 placeholder-white/80 text-base"
+        className="bg-transparent border-b border-white/50 focus:border-white outline-none py-3 placeholder-white/80 text-base"
       />
       <textarea
         placeholder="Message"
         rows={4}
-        className="w-full bg-transparent border-b border-white/50 focus:border-white outline-none py-3 placeholder-white/80 resize-none text-base"
+        className="bg-transparent border-b border-white/50 focus:border-white outline-none py-3 placeholder-white/80 resize-none text-base"
       ></textarea>
 
       <button
         type="submit"
-        className="w-full sm:w-auto px-8 mt-3 py-2 rounded-full text-base font-medium border border-white text-white hover:bg-white hover:!text-black transition-colors duration-300 cursor-pointer"
+        className="px-8 mt-3 py-2 rounded-full text-base font-medium border border-white text-white hover:bg-white hover:!text-black transition-colors duration-300 w-fit cursor-pointer"
       >
         Send
       </button>
